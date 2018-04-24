938d5c913d13ead2a942067dd1f22c4ec57145b1
Vadym Matsishevskyi <25311427+vam-google@users.noreply.github.com>
2018-04-24 14:54:38 -0700

    Bump version to 0.0.9 (#33)

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
     sys.exit()
 
-version = '0.0.8'
+version = '0.0.9'
 
 install_requires = [
     'pystache >= 0.5.4',
