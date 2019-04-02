6948f7fe607bb269c75025343b82cddc3223b9fb
Andrea Lin <asdf.lina@gmail.com>
2019-04-02 14:34:01 -0700

    uncomment tests

--- test/test_config_parsing.py
+++ test/test_config_parsing.py
@@ -21,35 +21,35 @@ GAPIC_CONFIG_V1_PATH = os.path.join(TEST_DIR, 'testdata/library_gapic_v1.yaml')
 
 
 class TestConfigParsing(TestCase):
-    # def test_config_parsing(self):
-    #     gapic_config = gapic_utils.read_from_gapic_yaml(GAPIC_CONFIG_PATH)
-    #     self.assertEqual(1, len(gapic_config.fixed_collections))
-    #     self.assertEqual(
-    #         "deleted_book",
-    #         gapic_config.fixed_collections['deleted_book'].entity_name)
-    #     self.assertEqual(
-    #         "_deleted-book_",
-    #         gapic_config.fixed_collections['deleted_book'].fixed_value)
-    #     self.assertEqual(
-    #         "deleted_book",
-    #         gapic_config.fixed_collections['deleted_book'].java_entity_name)
-    #
-    # def test_identify_single_and_fixed_entities(self):
-    #     project = {'name_pattern': 'projects/*', 'entity_name': 'project'}
-    #     deleted_book = {'name_pattern': '_deleted-book_',
-    #                     'entity_name': 'deleted_book'}
-    #     shelf = {'name_pattern': 'shelves/*/books/*',
-    #              'entity_name': 'shelf'}
-    #     book = {'name_pattern': 'book/{book_id}',
-    #             'entity_name': 'book'}
-    #     all_resource_names = [project, deleted_book, shelf, book]
-    #     (singles, fixies) = gapic_utils.find_single_and_fixed_entities(
-    #         all_resource_names)
-    #
-    #     self.assertIn(project, singles)
-    #     self.assertIn(deleted_book, fixies)
-    #     self.assertIn(shelf, singles)
-    #     self.assertIn(book, singles)
+    def test_config_parsing(self):
+        gapic_config = gapic_utils.read_from_gapic_yaml(GAPIC_CONFIG_PATH)
+        self.assertEqual(1, len(gapic_config.fixed_collections))
+        self.assertEqual(
+            "deleted_book",
+            gapic_config.fixed_collections['deleted_book'].entity_name)
+        self.assertEqual(
+            "_deleted-book_",
+            gapic_config.fixed_collections['deleted_book'].fixed_value)
+        self.assertEqual(
+            "deleted_book",
+            gapic_config.fixed_collections['deleted_book'].java_entity_name)
+
+    def test_identify_single_and_fixed_entities(self):
+        project = {'name_pattern': 'projects/*', 'entity_name': 'project'}
+        deleted_book = {'name_pattern': '_deleted-book_',
+                        'entity_name': 'deleted_book'}
+        shelf = {'name_pattern': 'shelves/*/books/*',
+                 'entity_name': 'shelf'}
+        book = {'name_pattern': 'book/{book_id}',
+                'entity_name': 'book'}
+        all_resource_names = [project, deleted_book, shelf, book]
+        (singles, fixies) = gapic_utils.find_single_and_fixed_entities(
+            all_resource_names)
+
+        self.assertIn(project, singles)
+        self.assertIn(deleted_book, fixies)
+        self.assertIn(shelf, singles)
+        self.assertIn(book, singles)
 
     def test_explicit_fixed_name_config_parsing(self):
         gapic_config = gapic_utils.read_from_gapic_yaml(GAPIC_CONFIG_V1_PATH)
