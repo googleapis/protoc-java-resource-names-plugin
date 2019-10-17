0aa3c08c96509a0479e2a83b2a982d57bca25756
Hanzhen Yi <hzyi@google.com>
2019-10-17 12:17:03 -0700

    Release 0.0.18

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
     sys.exit()
 
-version = '0.0.17'
+version = '0.0.18'
 
 install_requires = [
     'chevron >= 0.13.1',
