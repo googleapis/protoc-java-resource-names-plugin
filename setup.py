64dafb71ea9a385a8da89989f0b9dab925bf4610
Hanzhen Yi <hzyi@google.com>
2020-04-10 10:25:42 -0700

    release 0.0.25 (#85)

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
     sys.exit()
 
-version = '0.0.24'
+version = '0.0.25'
 
 install_requires = [
     'chevron >= 0.13.1',
